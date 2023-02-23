9937615694d9390ca1ce327a5ba4359c7fe30e06
jonstieglitz <jonathan.stieglitz@transferwise.com>
2023-02-23 17:01:06 +0000

    hover styling and nav row

--- components/navitem.jsx
+++ components/navitem.jsx
@@ -2,12 +2,15 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-const NavItem = ({ source, width, href }) => {
-
+const NavItem = ({ source, styling, width, href }) => {
   return (
-    <Link class={styles.nav__link} href={href} style={{paddingLeft: "15px"}}>
-          <Image src={source} width={width} height="30"/>
-     </Link>
+    <Link
+      class={`${styles.nav__link} ${styles[styling]}`}
+      href={href}
+      style={{ paddingLeft: "15px" }}
+    >
+      <Image src={source} width={width} height="30" />
+    </Link>
   );
 };
 
